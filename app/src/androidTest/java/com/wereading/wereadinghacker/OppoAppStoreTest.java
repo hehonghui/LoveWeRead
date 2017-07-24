b8828974435713d8582713c5a45e9ac45941d6eb
mr.simple <bboyfeiyu@gmail.com>
2017-07-24 17:13:09 +0800

    优化效率

--- app/src/androidTest/java/com/wereading/wereadinghacker/OppoAppStoreTest.java
+++ app/src/androidTest/java/com/wereading/wereadinghacker/OppoAppStoreTest.java
@@ -12,16 +12,12 @@ import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
-import android.widget.ImageView;
 
 import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -45,17 +41,15 @@ public class OppoAppStoreTest {
         // Initialize UiDevice instance
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
 
-        // Start from the home screen
-        mDevice.pressHome();
-
-        checkNoCrashDialog();
-
-        // Wait for launcher
-        final String launcherPackage = mDevice.getLauncherPackageName();
-        assertThat(launcherPackage, notNullValue());
-        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
-
-        checkRemoveAd();
+//        // Start from the home screen
+//        mDevice.pressHome();
+//
+//        checkNoCrashDialog();
+//
+//        // Wait for launcher
+//        final String launcherPackage = mDevice.getLauncherPackageName();
+//        assertThat(launcherPackage, notNullValue());
+//        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
 
         // Launch the app
         Context context = InstrumentationRegistry.getContext();
@@ -69,50 +63,30 @@ public class OppoAppStoreTest {
     }
 
     private void checkNoCrashDialog() {
-        try {
-            UiObject okObj = mDevice.findObject(new UiSelector().resourceId("android:id/button1").text("OK"));
-            while (isValidObject(okObj)) {
-                okObj = mDevice.findObject(new UiSelector().resourceId("android:id/button1").text("OK"));
-                if ( isValidObject(okObj)) {
-                    okObj.click();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void checkRemoveAd()  {
-        UiObject okObj = mDevice.findObject(new UiSelector().text("推广"));
-        try {
-            // 是否有广告
-            if (isValidObject(okObj)) {
-                UiObject closeObj = mDevice.findObject(new UiSelector().className(ImageView.class).index(1));
-                // 关闭广告
-                if ( isValidObject(closeObj)) {
-                    closeObj.click();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            UiObject okObj = mDevice.findObject(new UiSelector().resourceId("android:id/button1").text("OK"));
+//            while (isValidObject(okObj)) {
+//                okObj = mDevice.findObject(new UiSelector().resourceId("android:id/button1").text("OK"));
+//                if ( isValidObject(okObj)) {
+//                    okObj.click();
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 
     @Test
     public void startAppStore() throws Exception {
-//        if (isPkgInstalled(InstrumentationRegistry.getTargetContext(), TARGET_PKG)) {
-//            // 先确保删除老的包
-//            mDevice.executeShellCommand("pm uninstall " + TARGET_PKG);
-//        }
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         checkNoCrashDialog();
 
         UiObject searchTv = mDevice.findObject(new UiSelector().resourceId("com.oppo.market:id/ll_bg"));
         // 等待找到搜索框
         if ( !isValidObject(searchTv)) {
-            Thread.sleep(3000);
+            Thread.sleep(2000);
             searchTv = mDevice.findObject(new UiSelector().resourceId("com.oppo.market:id/ll_bg"));
         }
         // 进入搜索页面
@@ -168,53 +142,27 @@ public class OppoAppStoreTest {
             packageInfo = context.getPackageManager().getPackageInfo(pkgName, 0);
         } catch (PackageManager.NameNotFoundException e) {
             packageInfo = null;
-            e.printStackTrace();
+//            e.printStackTrace();
         }
         return packageInfo != null;
     }
 
     private void waitingForDownload() throws Exception {
         int times = 0;
-
-        // 模拟器测试逻辑
-//        while (times++ <= 12) {
-//            // 外部安装
-//            isInstalledFromOutside();
-//            // 自动安装
-//            if (isPkgInstalled(InstrumentationRegistry.getTargetContext(), TARGET_PKG) ) {
-//                break;
-//            }
-//            Thread.sleep(10 * 1000);
-//        }
-//        openNewsDog("com.android.packageinstaller:id/launch_button");
-
         // oppo 逻辑
-        while (times++ <= 20) {
+        while (times++ <= 45) {
             // 自动安装
             if (isPkgInstalled(InstrumentationRegistry.getTargetContext(), TARGET_PKG) ) {
                 break;
             }
-            Thread.sleep(5 * 1000);
+            Thread.sleep(2 * 1000);
         }
 
         checkNoCrashDialog();
 
-        Thread.sleep(5 * 1000);
+        Thread.sleep(3 * 1000);
         // open newsdog
         openNewsDog("com.oppo.market:id/button_download");
-
-        try {
-            allowPermissionsForCoolpadF1();
-
-            // todo : coolpad 大神的包中执行不了卸载功能, 因此转移到shell 中.
-            // 打开过NewsDog之后再卸载, 然后重复这个过程
-//            mDevice.executeShellCommand("pm uninstall " + TARGET_PKG);
-//
-//            // force stop market app
-//            mDevice.executeShellCommand("am force-stop com.oppo.market");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 
@@ -222,69 +170,6 @@ public class OppoAppStoreTest {
         UiObject openObject = mDevice.findObject(new UiSelector().resourceId(openUiObjName));
         if ( isValidObject(openObject) ) {
             openObject.click();
-        }
-    }
-
-    /**
-     * 可以分出两套代码执行.
-     * 1. 自动安装.
-     * 2. 外部安装.
-     *
-     * @throws UiObjectNotFoundException
-     */
-    private void isInstalledFromOutside() throws UiObjectNotFoundException {
-        try {
-            UiObject marketInstallObject = mDevice.findObject(new UiSelector().resourceId("com.oppo.market:id/button_download"));
-            // 如果下载完成会跳转到外部安装程序, 此时点击外部安装程序的"Next"按钮进行安装; 否则再点击一下 下载按钮,继续下载.
-            if ( isValidObject(marketInstallObject) ) {
-                marketInstallObject.click();
-                Thread.sleep(100);
-                // 查看是否安装完成
-                UiObject outsideInstallObject = mDevice.findObject(new UiSelector().resourceId("com.android.packageinstaller:id/ok_button").text("Next"));
-                if ( !isValidObject(outsideInstallObject)) {
-                    // 没有下载完成, 再次点击继续下载. 否则跳出 if 条件 ,继续往下执行, 点击"Next"进行安装
-                    marketInstallObject.click();
-                    return;
-                }
-            }
-
-            // 外部安装程序
-            UiObject outsideInstallObject = mDevice.findObject(new UiSelector().resourceId("com.android.packageinstaller:id/ok_button").text("Next"));
-            // 外部安装程序
-            if ( isValidObject(outsideInstallObject) ) {
-                outsideInstallObject.click();
-                Thread.sleep(1000);
-                // install
-                UiObject installObject = mDevice.findObject(new UiSelector().resourceId("com.android.packageinstaller:id/ok_button").text("Install"));
-                if ( isValidObject(installObject) ) {
-                    installObject.click();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * 允许权限
-     * @throws UiObjectNotFoundException
-     */
-    private void allowPermissionsForCoolpadF1() throws UiObjectNotFoundException {
-        try {
-//            UiObject allowObject = mDevice.findObject(new UiSelector().resourceId("android:id/button1").text("Allow"));
-//            if ( isValidObject(allowObject) ) {
-//                allowObject.click();
-//                Thread.sleep(1000);
-//            }
-//
-//            UiObject allowObject2 = mDevice.findObject(new UiSelector().resourceId("android:id/button1").text("Allow"));
-//            if ( isValidObject(allowObject2) ) {
-//                allowObject2.click();
-//                Thread.sleep(2000);
-//            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
