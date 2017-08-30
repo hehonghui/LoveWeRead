1dd4511c95a68360cb66b3c51eb2ec4ebc2e1f5f
mr.simple <bboyfeiyu@gmail.com>
2017-08-30 12:13:22 +0800

    add test

--- app/src/main/java/com/wereading/wereadinghacker/MainActivity.java
+++ app/src/main/java/com/wereading/wereadinghacker/MainActivity.java
@@ -3,15 +3,8 @@ package com.wereading.wereadinghacker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -39,31 +32,38 @@ public class MainActivity extends AppCompatActivity {
         mInfoTv.append("; Color getRomVersion : " + getSystemProperty("ro.build.display.id") + "\n" );
         mInfoTv.append("; Color Os Version: " + ColorOSVersionUtil.getColorOsVersion() + "\n" );
 
-        new Thread() {
-            @Override
-            public void run() {
-                sendRequest();
-            }
-        }.start();
     }
 
-
-    private void sendRequest() {
-        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext()) ;
-        StringRequest request = new StringRequest("https://app.appsflyer.com/com.newsdog?pid=oppo&c=000&af_r=http://www.baidu.com", new Response.Listener<String>() {
-
-            @Override
-            public void onResponse(String response) {
-                Log.e("", "### request af resp : " + response) ;
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                Log.e("", "### request af error : " + volleyError) ;
-            }
-        }) ;
-        requestQueue.add(request) ;
-    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//
+//        mTextView1 = new TextView(this) ;
+//        mTextView1.setText("Hello");
+//
+//        // 添加到视图层级中
+//        ((ViewGroup)getWindow().getDecorView()).addView(mTextView1);
+//
+//        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                new Thread() {
+//                    @Override
+//                    public void run() {
+//                        mTextView2 = new TextView(MainActivity.this) ;
+//                        mTextView2.setText("This is text view 2");
+//
+//                        mTextView1.setText("Changed");
+//                    }
+//                }.start();
+//            }
+//        }, 1000);
+//    }
+//
+//    TextView mTextView1 ;
+//    TextView mTextView2 ;
 
     public static String getSystemProperty(String property) {
         String value = "";
@@ -84,46 +84,4 @@ public class MainActivity extends AppCompatActivity {
         return value;
     }
 
-
-    /**
-     *
-     *   ( 2187): ### SystemProperties get => gsm.operator.alpha
-     I/Xposed  ( 2187): ### SystemProperties get => ro.build.display.id
-     I/Xposed  ( 2187): ### SystemProperties get => ro.build.version.opporom
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => ro.build.display.id
-     I/Xposed  ( 2187): ### SystemProperties get => debug.sqlite.journalmode
-     I/Xposed  ( 2187): ### SystemProperties get => debug.sqlite.syncmode
-     I/Xposed  ( 2187): ### SystemProperties get => debug.second-display.pkg
-     I/Xposed  ( 2187): ### SystemProperties get => ro.config.low_ram
-     I/Xposed  ( 2187): ### SystemProperties get => ro.config.low_ram
-     I/Xposed  ( 2187): ### SystemProperties get => ro.config.low_ram
-     I/Xposed  ( 2187): ### SystemProperties get => ro.config.low_ram
-     I/Xposed  ( 2187): ### SystemProperties get => ro.config.low_ram
-     I/Xposed  ( 2187): ### SystemProperties get => gsm.operator.alpha
-     I/Xposed  ( 2187): ### SystemProperties get => ro.build.display.id
-     I/Xposed  ( 2187): ### SystemProperties get => ro.build.version.opporom
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => ro.yunos.version
-     I/Xposed  ( 2187): ### SystemProperties get => java.vm.name
-     I/Xposed  ( 2187): ### SystemProperties get => debug.hwui.profile
-     I/Xposed  ( 2187): ### SystemProperties get => ro.config.low_ram
-     I/Xposed  ( 2187): ### SystemProperties get => debug.sqlite.journalmode
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => debug.sqlite.syncmode
-     I/Xposed  ( 2187): ### SystemProperties get => debug.hwui.profile
-     I/Xposed  ( 2187): ### SystemProperties get => gsm.operator.alpha
-     I/Xposed  ( 2187): ### SystemProperties get => ro.build.display.id
-     I/Xposed  ( 2187): ### SystemProperties get => ro.build.version.opporom
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     I/Xposed  ( 2187): ### SystemProperties get => ro.config.low_ram
-     I/Xposed  ( 2187): ### SystemProperties get => persist.sys.oppo.region
-     */
 }
